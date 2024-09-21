606b602e0b7a45be0a8ebd58999524a8508e6b5d
wallred7 <157505221+wallred7@users.noreply.github.com>
2024-09-21 11:37:36 -0700

    Changed the trainer.py args and added get_latest_ckpt function.

--- src/runner.py
+++ src/runner.py
@@ -1,10 +1,31 @@
+import os
+
 from model.training import train
 from model.inference import run_inference
 from model.evaluation import evaluate_output
+from model.model_service import get_latest_checkpoint
 from config.config import settings
 
-for i in settings.lang_abrv:
-    fine_tuned_model = train() # args: train_file, eval_file, train_lang (i), num_train_records
-    translations_files = run_inference(i, fine_tuned_model)
-    evaluate_output(translations_files)
+def main():
+    for lang in settings.lang_abrv:
+        target_languages = {
+                'pt-br': 'Brazilian Portuguese',
+                'cs': 'Czech',
+                'de': 'German',
+                'fi': 'Finnish',
+                'ko': 'Korean'
+            }
 
+        target_language = target_languages[lang]
+
+        run_name = f"{lang}_{settings.model_name.replace('.','_')}"
+        train_file = os.path.join(settings.training_data_path, f'train_dataset.{lang}')
+        eval_file = os.path.join(settings.reference_data_path, f'eval_dataset.{lang}') # DEV set
+
+        fine_tuned_model = train(train_file, eval_file, lang, run_name)
+        fine_tuned_model_ckpt = get_latest_checkpoint(fine_tuned_model)
+        translations_files = run_inference(lang, fine_tuned_model_ckpt, run_name)
+        evaluate_output(translations_files, lang, run_name)
+
+if __name__=="__main__":
+    main() 
